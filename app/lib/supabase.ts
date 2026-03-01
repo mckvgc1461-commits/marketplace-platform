d4e7ad4ddebb88d7dec59432a74e86c0082fb7c1
Kerem <kerem@marketplace.com>
2026-03-02 00:51:47 +0300

    Supabase key duzeltildi

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://khlremgxaifqxnspaemf.supabase.co";
-const supabaseAnonKey = "sb_publishable_fG76ZEKDzEEr00jmmu20Qw_ptCCZte5";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtobHJlbWd4YWlmcXhuc3BhZW1mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzOTYzNjgsImV4cCI6MjA4Nzk3MjM2OH0.oO45X8NlXNA77qVD1fm8wZC-y_evl8-l98EcPFHzdd8";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
