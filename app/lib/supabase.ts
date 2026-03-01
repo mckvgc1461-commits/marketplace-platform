e9b2a5b2e275bdde6a3c2ffd51bd584c892766f3
Kerem <kerem@marketplace.com>
2026-03-02 00:46:48 +0300

    Tum ayarlar duzeltildi

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://khlremgxaifqxnspaemf.supabase.co";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "sb_publishable_fG76ZEkOzEEr00jmmu20Qw_ptCCZ";
+const supabaseUrl = "https://khlremgxaifqxnspaemf.supabase.co";
+const supabaseAnonKey = "sb_publishable_fG76ZEKDzEEr00jmmu20Qw_ptCCZte5";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
